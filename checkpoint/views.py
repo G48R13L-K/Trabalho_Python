2f2163d212a625f2a3451498f68fcc07eccc980d
Daniel Maus <hakumennomodo@gmail.com>
2026-02-18 21:29:48 -0300

    Imports da home

--- checkpoint/views.py
+++ checkpoint/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpR, HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, this is the home page of the checkpoint app!")
